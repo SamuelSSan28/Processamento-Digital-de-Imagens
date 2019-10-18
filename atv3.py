606bc5d6af775be32047d26e0226dac3bedbda12
Samuel <samuelssan28@gmail.com>
2019-10-17 21:15:51 -0300

    Pratica 3

--- atv3.py
+++ atv3.py
@@ -1,51 +1,67 @@
 import numpy as np
 import cv2
 
-#leitura das imagens
-img = cv2.imread('morfologia.png',0)
+# leitura das imagens
+img = cv2.imread('morfologia.png', 0)
 
-median = cv2.medianBlur(img,5)
+median = cv2.medianBlur(img, 5)
 
-ret,binaria = cv2.threshold(median,200,255,cv2.THRESH_BINARY)
+ret, binaria = cv2.threshold(median, 200, 255, cv2.THRESH_BINARY)
 
-#kernelD = np.ones((3,3), np.uint8)
-kernel = np.ones((3,3), np.uint8)
-
-
+# kernelD = np.ones((3,3), np.uint8)
+kernel = np.ones((3, 3), np.uint8)
 
 print(kernel)
-dilatacao = cv2.erode(binaria, kernel , iterations=6)#destaca partes pretas
-dilatacao = cv2.dilate(dilatacao,kernel, iterations=5)
-for i in range(h):
-    for j in range(l):
-        if dilatacao[i][j] == 0:
-            dilatacao[i][j] = 255
-        else:
-            dilatacao[i][j] = 0
-
-kernel = kernel = np.ones((11,11), np.uint8)
-h,l = dilatacao.shape
-
-for i in range(h):
-    for j in range(l):
-        if dilatacao[i][j] == 0:
-            dilatacao[i][j] = 255
-        else:
-            dilatacao[i][j] = 0
-output_image = cv2.morphologyEx(dilatacao, cv2.MORPH_HITMISS, kernel)
+erosao = cv2.erode(binaria, kernel, iterations=6)  # destaca partes pretas
+dilatacao = cv2.dilate(erosao, kernel, iterations=5)
 
 
-rate = 50
-kernel = (kernel + 1) * 127
-kernel = np.uint8(kernel)
-kernel = cv2.resize(kernel, None, fx = rate, fy = rate, interpolation = cv2.INTER_NEAREST)
+# find contours
+contours, hierarchy = cv2.findContours(dilatacao, cv2.RETR_TREE, \
+                                            cv2.CHAIN_APPROX_SIMPLE)
 
-cv2.imshow("kernel", kernel)
-cv2.moveWindow("kernel", 0, 0)
+# create hull array for convexHull points
+hull = []
 
-cv2.imshow("Hit or Miss", output_image)
-cv2.moveWindow("Hit or Miss", 500, 200)
+# calculate points for each contour
+for i in range(len(contours)):
+    hull.append(cv2.convexHull(contours[i], False))
 
-cv2.imshow("Origem", dilatacao)
+# create an empty black image
+drawing = np.zeros((dilatacao.shape[0], dilatacao.shape[1], 3), np.uint8)
+
+# draw contours and hull points
+for i in range(len(contours)):
+    color_contours = (100, 255, 45)  # color for contours
+    color = (255, 255, 255)  # color for convex hull
+    # draw contours
+    cv2.drawContours(drawing, contours, i, color_contours, 2, 8, hierarchy)
+    # draw convex hull
+    cv2.drawContours(drawing, hull, i, color, 2, 8)
+
+cv2.imshow("Output", drawing)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+size = np.size(img)
+skel = np.zeros(img.shape, np.uint8)
+
+ret, img = cv2.threshold(img, 127, 255, 0)
+element = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
+done = False
+
+while (not done):
+    eroded = cv2.erode(img, element)
+    temp = cv2.dilate(eroded, element)
+    temp = cv2.subtract(img, temp)
+    skel = cv2.bitwise_or(skel, temp)
+    img = eroded.copy()
+
+    zeros = size - cv2.countNonZero(img)
+    if zeros == size:
+        done = True
+
+cv2.imshow("skel", skel)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
